8a491e319b7e1f67738a0e07c00bb7ace7a690c1
PolarisTLX <paul.a.rail@gmail.com>
2018-07-03 22:03:35 -0400

    Finished user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+    # this is equivalent to:
+    # @user = User.find(1)
+    # Technically, params[:id] is the string "1", but find is smart enough to convert this to an integer.
+  end
+
+  def create
+    # @user = User.new(params[:user])
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to my app!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: {
+        name: "",
+        email: "user@invalid",
+        password: "pass",
+        password_confirmation: "word" } }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: { user: {
+        name: "valid",
+        email: "user@valid.com",
+        password: "password",
+        password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
+end
