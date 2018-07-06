61987fe4e509fc7bb64af5374d99b5fcfbe9d064
PolarisTLX <paul.a.rail@gmail.com>
2018-07-06 11:16:33 -0400

    finished adding remember-me feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   # alternative method with '!' :
   # before_save { email.downcase! }
@@ -19,6 +21,29 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token for remember-me
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches teh digest
+  def authenticated?(remember_token)
+    if remember_digest.nil?
+      false
+    else
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
 end
